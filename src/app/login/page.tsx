c2d5246b025c4f1d00b1f061f3989be76c111080
Johnmark Banisilon <zionsploit@gmail.com>
2025-07-15 00:17:24 +0800

    fix: :bug: Address unintended behavior on login page
    Correct an issue on the login page related to session handling.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import { get_auth_session } from "@/utils/helper";
 export default async function Login() {
     const session_data = await get_auth_session(cookies)
 
-    if (session_data == null) {
+    if (session_data !== null) {
         redirect("/dashboard/home")
     }
 
