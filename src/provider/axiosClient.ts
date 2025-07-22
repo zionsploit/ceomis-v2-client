cc7b9990295eb7ef11312af618ef75dc7fc1e1e9
Johnmark Banisilon <zionsploit@gmail.com>
2025-07-22 21:29:49 +0800

    refactor: :recycle: Standardize API base URL in Axios client
    Removes the `apiDockerURL` constant and updates the `axiosClient` instance to use `apiURL` as its default `baseURL`.

--- src/provider/axiosClient.ts
+++ src/provider/axiosClient.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 export const apiURL = process.env.NEXT_PUBLIC_API_URL
-export const apiDockerURL = process.env.NEXT_PUBLIC_DOCKER_API_URL
 
 export type AxiosClientRequestHeaders = { auth: string, sid: string}
 
 export const axiosClient = (headers: AxiosClientRequestHeaders) => {
 
     const instance = axios.create({
-        baseURL: apiDockerURL,
+        baseURL: apiURL,
         timeout: 100000,
         fetchOptions: {
             caches: 'no-store'
